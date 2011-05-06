6a4367a089a4ed920e8dc9a70f8926263914bd2f
Rafael Carício <rafael.jacinto@gmail.com>
2011-05-05 23:04:47 -0300

    First commit.

---
+++ django_pyvows/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# django-pyvows extensions
+# https://github.com/rafaelcaricio/django-pyvows
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2011 Rafael Caricio rafael@caricio.com
+
+__version__ = (0, 0, 1)
