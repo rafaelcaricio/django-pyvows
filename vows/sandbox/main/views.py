e3c4b9c548e396a58b8623f899fceb5facd2f52b
Bernardo Heynemann <heynemann@gmail.com>
2011-05-07 23:33:25 -0300

    Starting django pyvows

---
+++ vows/sandbox/main/views.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# django-pyvows extensions
+# https://github.com/rafaelcaricio/django-pyvows
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2011 Rafael Caricio rafael@caricio.com
+
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('hello world')
